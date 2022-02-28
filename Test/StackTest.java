2c5ad28d166afb07861f4ea36f2d6fd65179e4aa
91093467 <91093467@mvusd.net>
2022-02-28 08:45:17 -0800

    Single Linked List

---
+++ Test/StackTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StackTest {
+    @Test
+    public void StackValue() {
+        Stack stack = new Stack();
+    }
+}
---
+++ src/MainClass.java
@@ -0,0 +1,7 @@
+public class MainClass {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(5);
+        stack.push(10);
+    }
+}
